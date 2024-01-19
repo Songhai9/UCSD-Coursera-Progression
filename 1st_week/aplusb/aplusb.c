d48c5aa211b5eb98b5a34c0a05c2f07b61b2147a
Oumarou Maïga <doosix@outlook.fr>
2024-01-19 17:18:17 +0100

    First exercise : print a sum of two integers

---
+++ 1st_week/aplusb/aplusb.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b;
+    scanf("%i", &a);
+    scanf("%i", &b);
+
+    int c = a + b;
+
+    printf("%i\n", c);
+
+    return 0;
+}
