0a520cdba7c75cea6b4057ac3d304a1da81c5328
Oumarou Maïga <doosix@outlook.fr>
2024-01-22 00:36:03 +0100

    Last digit of a fibonacci number

--- algorithmic_toolbox/2nd_week/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
+++ algorithmic_toolbox/2nd_week/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -1,16 +1,34 @@
-def fibonacci_last_digit(n):
+def find_cycle():
+    previous = 0
+    current = 1
+    iterator = 0
+
+    while True:
+        previous, current = current, (previous + current) % 10
+        iterator += 1
+        if previous == 0 and current == 1:
+            break
+
+    return iterator
+
+
+def fibonacci_last_digit_fast(n):
     if n <= 1:
         return n
 
     previous = 0
     current = 1
 
-    for _ in range(n - 1):
-        previous, current = current, previous + current
+    last_digit = [0] * 60
+    last_digit[1] = 1
 
-    return current % 10
+    for index in range(2, len(last_digit)):
+        (previous, current) = (current, (previous + current) % 10)
+        last_digit[index] = current
+
+    return last_digit[n % 60]
 
 
 if __name__ == '__main__':
     n = int(input())
-    print(fibonacci_last_digit(n))
+    print(fibonacci_last_digit_fast(n))
