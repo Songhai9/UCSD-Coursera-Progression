afbfa62c6e3f8391286e92ec7f9bbf3b84bd3fea
Oumarou Maïga <doosix@outlook.fr>
2024-01-22 00:44:21 +0100

    GCD

---
+++ algorithmic_toolbox/2nd_week/3_greatest_common_divisor/gcd.py
@@ -0,0 +1,20 @@
+def gcd(a, b):
+    current_gcd = 1
+    for d in range(2, min(a, b) + 1):
+        if a % d == 0 and b % d == 0:
+            if d > current_gcd:
+                current_gcd = d
+
+    return current_gcd
+
+
+def euclid_gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return euclid_gcd(b, a % b)
+
+
+if __name__ == "__main__":
+    a, b = map(int, input().split())
+    print(euclid_gcd(a, b))
